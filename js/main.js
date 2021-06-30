110c766b518764e3fa03e65e9c077d67291232e2
AbdQaddora <abdqaddora732001@outlook.com>
2021-06-30 11:58:22 +0300

    update seb7a page

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
+var arr = Array("سبحان الله" , "الحمد لله" , "لا إله الا الله" , "الله أكبر");
+var index = 0;
+document.getElementById("buttonText").innerText = arr[index];
 function increasCounter() {
     counter = document.getElementById("counter").innerText;
     counter++;
-    vibreate(100);
+    vibreate(70);
+    index = (index + 1) % 4;
+    document.getElementById("buttonText").innerText = arr[index];
     document.getElementById("counter").innerText = counter;
 }
 
