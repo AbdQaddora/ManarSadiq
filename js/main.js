a0b6e11c3361a623c347f22ad80ab7777895e31a
AbdQaddora <abdqaddora732001@outlook.com>
2021-06-30 18:01:46 +0300

    edit

--- js/main.js
+++ js/main.js
@@ -16,7 +16,7 @@ function increasCounter() {
             document.getElementById("outterCircle").style = "clip-path: polygon(50% 0%, 100% 0, 100% 49%, 100% 100%, 50% 100%, 0 100%, 0 52%, 50% 50%);";
             break;
         case 0:
-            document.getElementById("outterCircle").style = "clip-path: polygon(50% 0%, 100% 0, 100% 50%, 100% 100%, 50% 100%, 0 100%, 0 50%, 0 0);";
+            document.getElementById("outterCircle").style = "clip-path: polygon(50% 30%, 68% 30%, 60% 47%, 60% 59%, 43% 63%, 49% 69%, 49% 50%, 45% 39%);";
             counter = document.getElementById("counter").innerText;
             counter++;
             document.getElementById("counter").innerText = counter;
