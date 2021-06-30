25029ec71785cd8919795ab40ea28f6da033f69a
AbdQaddora <abdqaddora732001@outlook.com>
2021-06-30 13:30:07 +0300

    edit seb7a

--- js/main.js
+++ js/main.js
@@ -1,13 +1,28 @@
-var arr = Array("سبحان الله" , "الحمد لله" , "لا إله الا الله" , "الله أكبر");
+var arr = Array("سبحان الله", "الحمد لله", "لا إله الا الله", "الله أكبر");
 var index = 0;
 document.getElementById("buttonText").innerText = arr[index];
+
 function increasCounter() {
-    counter = document.getElementById("counter").innerText;
-    counter++;
-    vibreate(70);
     index = (index + 1) % 4;
     document.getElementById("buttonText").innerText = arr[index];
-    document.getElementById("counter").innerText = counter;
+    switch (index) {
+        case 1:
+            document.getElementById("outterCircle").style = "clip-path: polygon(50% 0%, 100% 0, 100% 50%, 100% 50%, 50% 50%, 50% 50%, 50% 50%, 50% 0);";
+            break;
+        case 2:
+            document.getElementById("outterCircle").style = "clip-path: polygon(50% 0%, 100% 0, 100% 50%, 100% 100%, 50% 100%, 49% 100%, 49% 50%, 50% 0);";
+            break;
+        case 3:
+            document.getElementById("outterCircle").style = "clip-path: polygon(50% 0%, 100% 0, 100% 49%, 100% 100%, 50% 100%, 0 100%, 0 52%, 50% 50%);";
+            break;
+        case 0:
+            document.getElementById("outterCircle").style = "clip-path: polygon(50% 0%, 100% 0, 100% 50%, 100% 100%, 50% 100%, 0 100%, 0 50%, 0 0);";
+            counter = document.getElementById("counter").innerText;
+            counter++;
+            document.getElementById("counter").innerText = counter;
+            vibreate(70);
+            break;
+    }
 }
 
 function vibreate(ms) {
